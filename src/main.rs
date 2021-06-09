85fb5ce13ec55df04adee58fe69b842602507fb5
Rudi <dyn-git@rudi-horn.de>
2021-06-09 14:59:36 +0000

    rewrite deinterlacing to have less per pixel operations

--- src/main.rs
+++ src/main.rs
@@ -82,52 +82,73 @@ fn main() {
             let mapping: &mut [u8] =
                 unsafe { std::slice::from_raw_parts_mut(map as *mut _, length as _) };
 
-
-            let xtiles = size.0 / tilesize;
             let mut i = 0;
-            for t in 0..total_tiles {
-                let mut tx = t % (xtiles * 2);
-                let altrow = tx >= xtiles;
-                if altrow {
-                    tx = 2 * xtiles - tx - 1;
+
+            let mut copy_px = |x, y| {
+                let color = Rgb([
+                    mapping[(i + 2) as usize],
+                    mapping[(i + 1) as usize],
+                    mapping[(i + 0) as usize],
+                ]);
+                unsafe {
+                    img.unsafe_put_pixel(x, y, color);
                 }
-                let ty = t / xtiles;
+                i = i + 4;
+            };
+            let mut copy_4_px = |x, y| {
+                copy_px(x, y);
+                copy_px(x + 1, y);
+                copy_px(x + 2, y);
+                copy_px(x + 3, y);
+            };
 
-                for j in 0..tilesize * tilesize {
-                    // each 32x32 tile is subdivided into small tiles of 16x16
-                    // and then tiles of 4x4. The 4x4 tiles are in order, the
-                    // 16x16 tiles go top left, bottom left, bottom right, top
-                    // right. This order seems to be reversed on the second row
+            let mut copy_4x4_px = |x, y| {
+                copy_4_px(x, y);
+                copy_4_px(x, y + 1);
+                copy_4_px(x, y + 2);
+                copy_4_px(x, y + 3);
+            };
 
-                    let not = |x| if x > 0 {0} else {1};
+            let mut copy_16x4_px = |x, y| {
+                copy_4x4_px(x, y);
+                copy_4x4_px(x + 4, y);
+                copy_4x4_px(x + 8, y);
+                copy_4x4_px(x + 12, y);
+            };
 
-                    let col1 = j % 4;
-                    let n = j / 4;
-                    let row1 = n % 4;
-                    let n = n / 4;
-                    let col2 = n % 4;
-                    let n = n / 4;
-                    let row2 = n % 4;
-                    let n = n / 4;
-                    let row3 = if n == 1 || n == 2 { 1 } else {0};
-                    let row3 = if altrow {not(row3)} else {row3};
-                    let col3 = n / 2;
-                    let col3 = if altrow {not(col3)} else {col3};
+            let mut copy_16x16_px = |x, y| {
+                copy_16x4_px(x, y);
+                copy_16x4_px(x, y + 4);
+                copy_16x4_px(x, y + 8);
+                copy_16x4_px(x, y + 12);
+            };
 
-                    let color = Rgb([
-                        mapping[(i + 2) as usize],
-                        mapping[(i + 1) as usize],
-                        mapping[(i + 0) as usize],
-                    ]);
+            for ytile in 0..tiles.1 {
+                if ytile % 2 == 0 {
+                    let mut copy_tile =
+                        |x, y| {
+                            copy_16x16_px(x, y);
+                            copy_16x16_px(x, y + 16);
+                            copy_16x16_px(x + 16, y + 16);
+                            copy_16x16_px(x + 16, y);
+                        };
 
-                    let xpos = col1 + col2 * 4 + col3 * 16 + tx * tilesize;
-                    let ypos = row1 + row2 * 4 + row3 * 16 + ty * tilesize;
+                    for xtile in 0..tiles.0 {
+                        copy_tile(xtile * tilesize, ytile * tilesize);
+                    }
+                } else {
+                    let mut copy_tile =|x, y| {
+                        copy_16x16_px(x + 16, y + 16);
+                        copy_16x16_px(x + 16, y);
+                        copy_16x16_px(x, y);
+                        copy_16x16_px(x, y + 16);
+                    };
 
-                    img.put_pixel(xpos, ypos, color);
-
-                    i = i + 4;
+                    for xtile in (0..tiles.0).rev() {
+                        copy_tile(xtile * tilesize, ytile * tilesize);
+                    }
                 }
-            }
+            };
 
             let cropped = img.sub_image(0, 0, size.0, size.1);
             cropped.to_image().save("screenshot.png").unwrap();
